0769b5fde04355cfbaa66fd07ddeb1594c809586
EvhenDemchenko <evgen7876951@gmail.com>
2023-01-25 16:44:52 +0200

    fix href bug

--- src/components/EmptyItemsInfo.tsx
+++ src/components/EmptyItemsInfo.tsx
@@ -5,7 +5,7 @@ export const EmptyItemsInfo = () => {
         <h1 className='isEmptyItems'> first Type VIN code in input...</h1>
         <h1 className='isEmptyItems'> press search button</h1>
         <h1 className='isEmptyItems'> enjoy results</h1>
-        <h1 className='isEmptyItems'> Also you can test app with <a href="https://vingenerator.org/">VIN generator</a>
+        <h1 className='isEmptyItems'> Also you can test app with <a  target={'_blank'} href="https://vingenerator.org/">VIN generator</a>
         </h1>
     </>)
 }
